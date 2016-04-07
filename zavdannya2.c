d37bc9d86dd9174a5914deb65171595ad7dac2db
breus32 <muller.09@mail.ru>
2016-04-08 00:21:50 +0300

    Added files via upload

---
+++ zavdannya2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+    int n, i;
+    
+	printf("Vvedite 4islo: ");
+	scanf("%d", &n);
+    
+	while (n != 0){
+        n=n/10;
+		i+=1;
+    }
+    printf("\nKoli4estvo cifr v 4isle = %d", i);
+    return 0;
+}
