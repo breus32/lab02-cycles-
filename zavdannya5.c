6307ccf6654eba70b837ba5aa31f228ca21f00f3
breus32 <muller.09@mail.ru>
2016-05-07 17:45:06 +0300

    Update zavdannya5.c

--- zavdannya5.c
+++ zavdannya5.c
@@ -3,16 +3,18 @@
 
 int main ()
 {
-	int i;
-	double sum = 0, factorial = 1, dodanok = 1, znam;
+    int i;
+    double sum = 0, factorial = 1, dodanok = 1, znam;
+    
+    for (i = 1; dodanok >= 0.01; i++) {
+    	factorial *= i;
+    	znam = pow (i, i);
+    	dodanok = factorial/ znam;
+    	sum = sum + dodanok;
+    	printf ("\n%lf", dodanok);
+    }
+    
+    printf("\nSumma ravna %lf", sum);
 	
-	for (i = 1; dodanok >= 0.01; i++) {     
-		factorial *= i;
-		znam = pow (i, i);
-		dodanok = factorial/ znam;
-		sum = sum + dodanok;
-		printf ("\n%lf", dodanok);
-	}
-	printf("\nSumma ravna %lf", sum);
-	return 0;
+    return 0;
 }
