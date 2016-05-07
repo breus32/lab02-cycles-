843a7c0182577ad41206e243418ed7faf0105986
breus32 <muller.09@mail.ru>
2016-05-07 17:38:40 +0300

    Update zavdannya4(3).c

--- zavdannya4(3).c
+++ zavdannya4(3).c
@@ -3,17 +3,17 @@
 
 int main ()
 {
-	int max;
-	double k, sum = 0;
-	
-	printf("max(k)= ");
-	scanf("%d", &max);
-	
-	for (k = 1; k <= max; k++) {
-		sum = 1 / pow ( k, 5);
-	}
-	
-	printf("\nSumma ravna %lf", sum);
-	
-	return 0;
+    int max;
+    double k, sum = 0;
+    
+    printf("max(k)= ");
+    scanf("%d", &max);
+    
+    for (k = 1; k <= max; k++) {
+    	sum = 1 / pow ( k, 5);
+    }
+    
+    printf("\nSumma ravna %lf", sum);
+    
+    return 0;
 }
