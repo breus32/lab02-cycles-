20a01e857383b5d39ea6dc3ed4de6329cfe6ca31
breus32 <muller.09@mail.ru>
2016-05-07 17:36:22 +0300

    Update zavdannya3.c

--- zavdannya3.c
+++ zavdannya3.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 int main() {
-	int n, m, r;
-	printf("Vvedite n:");
-	scanf("%d", &n);
-	printf("Vvedite m:");
-	scanf("%d", &m);
+   
+    int n, m, r;
 	
-	for ( ; n != 0; ){
-		r = m%n;
-		m = n;
-		n = r;
-	}
-	printf("NOD = %d", m);
+    printf("Vvedite n:");
+    scanf("%d", &n);
 	
-	return 0;
+    printf("Vvedite m:");
+    scanf("%d", &m);
+	
+    for ( ; n != 0; ){
+    	r = m%n;
+    	m = n;
+    	n = r;
+    }
+
+    printf("NOD = %d", m);
+    
+    return 0;
 }
