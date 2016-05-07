adbb03cfbc14b0a11d315b0cd4c0e0e3716aa124
breus32 <muller.09@mail.ru>
2016-05-07 17:33:33 +0300

    Update zavdannya1.c

--- zavdannya1.c
+++ zavdannya1.c
@@ -2,33 +2,35 @@
 
 int main ()
 {
-	int n, a, i;
-	printf("Vvedite n: ");
-	scanf("%d", &n);
+    int n, a, i;
+    
+    printf("Vvedite n: ");
+    scanf("%d", &n);
 	
+    printf("\nProstie 4isla: %d", n);
 	
-	printf("\nProstie 4isla: %d", n);
-	for (i = 1; i < n; i++) {
-		a = pros(i);
-		if (a == 1)
-			printf(" %d", i);
-	}
-	
+    for (i = 1; i < n; i++) {
+    	a = pros(i);
+    	if (a == 1){
+    		printf(" %d", i);
+    	}
+    }
 	return 0;
 }
 
-int pros (int n)
+int FindPrime (int n)
 {
-	int i, osta4a;
-	
-	i = 2;
-	
-	while (i < n) {
-		osta4a = n%i;
-		if (osta4a == 0)
-			return 0;
-		i++;
+    int i, osta4a;
+    
+    i = 2;
+    
+    while (i < n) {
+    	osta4a = n%i;
+    	if (osta4a == 0){
+    		return 0;
 	}
+	i++;
+    }
 	return 1;
 }
 
